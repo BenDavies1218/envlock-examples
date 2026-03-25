d09b4705682f4b01c2fe665f6c1fb00b790cc9df
Benjamin Davies <148213274+BenDavies1218@users.noreply.github.com>
2026-03-25 21:27:09 +1000

    chore: migrate apps/example from envlock to @envlock/next

--- apps/example/next.config.js
+++ apps/example/next.config.js
@@ -1,10 +1,9 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+import { withEnvlock } from "@envlock/next";
 import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
-
-export default config;
+export default withEnvlock(
+  {},
+  {
+    onePasswordEnvId: "ca6uypwvab5mevel44gqdc2zae",
+  },
+);
--- apps/example/src/env.js
+++ apps/example/src/env.js
@@ -1,42 +1,21 @@
-import { createEnv } from "@t3-oss/env-nextjs";
+import { createEnv } from "@envlock/next";
 import { z } from "zod";
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     DATABASE_URL: z.string().url(),
     API_SECRET: z.string().min(1),
   },
 
-  /**
-   * Client-side environment variables schema.
-   * Must be prefixed with `NEXT_PUBLIC_`.
-   */
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url(),
   },
 
-  /**
-   * Manual destructuring required for Next.js edge runtimes and client-side.
-   */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     API_SECRET: process.env.API_SECRET,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
-  emptyStringAsUndefined: true,
 });
